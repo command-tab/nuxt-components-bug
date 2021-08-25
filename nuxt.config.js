a5a4641189d9f902e570c27842809c1a9a1e0805
Collin Allen <collin@command-tab.com>
2021-08-25 13:03:24 -0700

    Add two scoped components

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,11 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components',
+    { path: '@hzzt/v-lazy-image', pathPrefix: false },
+    { path: '@command-tab/place-kitten', pathPrefix: false }
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <Tutorial/>
+  <div>
+    <div>Observe that @hzzt/v-lazy-image renders in dev and production builds:</div>
+    <v-lazy-image src="https://placekitten.com/400/200" />
+
+    <div>But @command-tab/place-kitten renders in dev, but does not render when doing <code>nuxt build</code>:</div> 
+    <place-kitten :width="400" :height="200" />
+  </div>
 </template>
 
 <script>
